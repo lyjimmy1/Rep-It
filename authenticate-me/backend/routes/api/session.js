4f2bf5783263d34c16c1102607565f1aef63719c
Jimmy Ly <lyjimmy19@gmail.com>
2021-05-20 16:04:05 -0400

    established logout functionality

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -32,6 +32,14 @@ router.post(
     }),
   );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 
 module.exports = router;
