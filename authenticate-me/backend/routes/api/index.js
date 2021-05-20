d392e2ee54c9649a97358392ae45611c479d75c4
Jimmy Ly <lyjimmy19@gmail.com>
2021-05-20 15:59:42 -0400

    established login routes

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
 
 
 router.post('/test', function(req, res) {
