09ea87087bb7cd83e20281f98dc4f19dfa5d0e9b
Jimmy Ly <lyjimmy19@gmail.com>
2021-05-21 16:59:10 -0400

    login form component

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -6,13 +6,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 import configureStore from './store';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
@@ -27,7 +32,7 @@ function Root() {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
