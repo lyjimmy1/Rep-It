d648ae92c5ce0d6530a2ad40353057c1fcbb3ff9
Jimmy Ly <lyjimmy19@gmail.com>
2021-08-12 12:21:56 -0400

    revamped splash page

--- authenticate-me/frontend/src/components/SpashPage/index.js
+++ authenticate-me/frontend/src/components/SpashPage/index.js
@@ -1,18 +1,45 @@
 import './SplashPage.css'
+import curling from '../../images/man-curling.jpg'
+import cleaning from '../../images/girl-cleaning-weight.jpg'
+import kettle from '../../images/work-out-in-a-gym-in-a-mask-sunday-edit.jpg'
+import plank from '../../images/guy-running-in-place.jpg'
 
 
 function Homepage(){
-    
-    return(
-    <div className="bg-img">
-        <div className="bg-img img1"></div>
-        <div className="bg-img img2"></div>
-        <div className="bg-img img3"></div>
-        <div className="bg-img img4"></div>
-        <div className="bg-img img5"></div>
-        <div className="bg-img img6"></div>
 
-        <div className="img7"></div>
+    return(
+    <div className="bg-container">
+        <div className="bg-text"></div>
+        <div className="splash-search">
+            <input className="search-input" type="text" placeholder="Find an exercise for you" />
+            <button className="search-btn">Search</button>
+        </div>
+        <div className="splash-footer">
+            <div className="splash-footer-container">
+                <h2 className="splash-header">
+                    Find the Best Exercise for You
+                </h2>
+                <div className="splash-footer-img-container">
+                    <div className="splash-footer-img-box">
+                        <img className="splash-footer-img" src={curling}/>
+                        <p>Bicep Curl</p>
+                    </div>
+                    <div className="splash-footer-img-box">
+                        <img className="splash-footer-img" src={cleaning}/>
+                        <p>Cleans</p>
+                    </div>
+                    <div className="splash-footer-img-box">
+                        <img className="splash-footer-img" src={kettle}/>
+                        <p>Kettlebell Swing</p>
+                    </div>
+                    <div className="splash-footer-img-box">
+                        <img className="splash-footer-img" src={plank}/>
+                        <p>Running Planks</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
     )
